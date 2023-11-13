7a092b9f21c1698b638e335e381056fe1a90e706
tommy-anderson <alexursu@planable.io>
2023-11-13 21:06:46 +0200

    👌 Fix stuff

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,32 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
+import { polyfillNode } from "esbuild-plugin-polyfill-node";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 
 module.exports = defineConfig({
   e2e: {
+    experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on(
+        "file:preprocessor",
+        createBundler({
+          alias: {
+            "meteor/random": "meteor-random-node",
+          },
+          plugins: [
+            polyfillNode({
+              polyfills: {
+                crypto: true,
+                stream: true,
+                path: true,
+              },
+              globals: {
+                buffer: true,
+                global: true,
+              },
+            }),
+          ],
+        })
+      );
     },
   },
 });
