297d2b82cc6939c338e2ad091facdc284df170d8
tommy-anderson <alexursu@planable.io>
2023-11-20 23:41:47 +0200

    👌 Store test results

---
+++ cypress/e2e/3-added-tests/passingTests3.cy.ts
@@ -0,0 +1,21 @@
+describe("all succeed 3", { tags: "added" }, () => {
+  it("succeed 1", () => {
+    expect(true).to.equal(true);
+  });
+
+  it("succeed 2", () => {
+    expect(true).to.equal(true);
+  });
+
+  it("succeed 3", () => {
+    expect(true).to.equal(true);
+  });
+
+  it("succeed 4", () => {
+    expect(true).to.equal(true);
+  });
+
+  it("succeed 5", () => {
+    expect(true).to.equal(true);
+  });
+});
